174661e17b3a02a7ec6ef54c935b5451921a097d
mugil <mugil@mugil>
2025-08-14 15:11:30 +0530

    day2 updated

---
+++ C2TC_corejavaprorams/src/day3/entity/ProductDemo.java
@@ -0,0 +1,30 @@
+package day3.entity;
+
+
+	import java.util.Scanner;
+
+	public class ProductDemo {
+		
+		public static void main(String[] args) {
+			
+			Product p1=new Product();
+			Product p2=new Product(102, "mouse", 250.4f);
+			
+			Scanner s=new Scanner(System.in);
+			
+			System.out.println("Enter the product details:");
+			
+			int sid=s.nextInt();
+			s.nextLine();
+			String prod_name= s.nextLine();
+			Float price=s.nextFloat();
+			
+			Product p3=new Product(sid, prod_name,price);
+			
+			System.out.println(p1);
+			System.out.println(p2);
+			System.out.println(p3);
+		}
+	}
+
+
